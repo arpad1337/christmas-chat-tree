05ab711e6b67d6d5ebd48d29107e4b813cba8959
Kiss <arpad1337@me.com>
2019-11-27 19:09:03 +0100

    Progress

--- src/common/store/communication/actions.js
+++ src/common/store/communication/actions.js
@@ -1,7 +1,7 @@
 import { GET_USERS, RECEIVE_MESSAGE, REMOVE_MESSAGE } from "./types";
 import baseURL from "../../baseURL";
 
-export const getUsers = () => async(dispatch, getState, api) => {
+export const getUsers = () => async (dispatch, getState, api) => {
     const { data: users } = await api.get("/users");
     dispatch({
         type: GET_USERS,
@@ -9,24 +9,39 @@ export const getUsers = () => async(dispatch, getState, api) => {
     });
 };
 // TASK 1: connection and getting data through SSE
-export const startCommunication = () => async(dispatch, getState, api) => {
-    // connect to the eventsource @ baseURL + "/communication"
-    // ...
-    // listen for messages (you can send a message to see a new message arrive)
-    // ...
-        // parse message.data
-        // ...
-        // switch
-            // if message type is 'users' dispatch a GET_USERS type action with data as payload
-            // { type: ..., payload: ... }
-            // ...
-            // if message type is message dispatch a RECEIVE_MESSAGE type action with data as paylaod
-            // case ...
-                // ...
-                // after data.expiresIn ms dispatch a REMOVE_MESSAGE type action with data.id as payload 
-                // ...
+export const startCommunication = () => async (dispatch, getState, api) => {
+    const eventSource = new EventSource(baseURL + '/communication');
+    eventSource.onmessage = (e) => {
+        console.log(e);
+        const { type, data } = JSON.parse(e.data);
+        switch(type) {
+            case 'users': {
+                dispatch({
+                    type: GET_USERS, 
+                    payload: data
+                });
+                break;
+            }
+            case 'message': {
+                dispatch({
+                    type: RECEIVE_MESSAGE, 
+                    payload: data
+                });
+                setTimeout(() => {
+                    dispatch({
+                        type: REMOVE_MESSAGE,
+                        payload: data.id
+                    });
+                }, data.expiresIn);
+                break;
+            }
+            default: {
+                return;
+            }
+        }
+    }
 };
 
-export const sendMessage = data => async(dispatch, getState, api) => {
+export const sendMessage = data => async (dispatch, getState, api) => {
     await api.post("/send-message", data);
 };
