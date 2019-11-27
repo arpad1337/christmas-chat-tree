19ae4fe99d0f60adad02041c0cb0f879fd474e6a
Kiss <arpad1337@me.com>
2019-11-27 20:06:46 +0100

    bug

--- src/client/renderer.js
+++ src/client/renderer.js
@@ -4,7 +4,6 @@ import { StaticRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { renderRoutes } from "./react-router-config";
 import serialize from "serialize-javascript";
-import { Helmet } from "react-helmet";
 import routes from "./routes";
 
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
